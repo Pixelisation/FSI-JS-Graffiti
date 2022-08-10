9abf30e92874673965484c267c0a6f8408dc30e4
torian austin <torian03@outlook.com>
2022-08-09 19:33:57 -0700

    change background-color

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Select some elements...
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
+
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius = '50px'
+}
+
+let DogName = document.querySelectorAll('.dog-name')
+for(let A = 0; A <DogName.length; A++){
+    DogName[A].style.background-color; 'red' 
+}
