600d132a6c1ec40fcd9918871ca3e9bdf557d91e
torian austin <torian03@outlook.com>
2022-08-09 19:35:59 -0700

    last change

--- index.js
+++ index.js
@@ -8,7 +8,4 @@ for(let i = 0; i < dogImages.length; i++) {
     dogImages[i].style.borderRadius = '50px'
 }
 
-let DogName = document.querySelectorAll('.dog-name')
-for(let A = 0; A <DogName.length; A++){
-    DogName[A].style.background-color; 'red' 
-}
+
